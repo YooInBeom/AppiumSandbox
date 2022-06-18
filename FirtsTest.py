78fa333c81c20212e3e644bc1246137c285856d6
inbeom <yib95@naver.com>
2022-06-19 01:33:40 +0900

    test

--- FirtsTest.py
+++ FirtsTest.py
@@ -14,3 +14,4 @@ driver.find_element(By.ID, 'com.sand.airmirror:id/tvLogin').click()
 
 search_element = driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.AutoCompleteTextView')
 search_element.set_text('yib95us@gmail.com')
+
