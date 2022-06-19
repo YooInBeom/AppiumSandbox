a2643cf27cbe73ba10c83ccdefc316c0aeef49bf
yooinbeom <yib950@gmail.com>
2022-06-19 16:38:14 +0900

    fix able to send os environ

--- FirtsTest.py
+++ FirtsTest.py
@@ -1,9 +1,10 @@
 import os
+import sys
 from appium import webdriver
 from selenium.webdriver.common.by import By
 
-ID = os.environ['ID']
-PASSWORD = os.environ['PASSWORD']
+GOOGLE_ID = os.environ['GOOGLE_ID']
+GOOGLE_PASSWORD = os.environ['GOOGLE_PASSWORD']
 
 desired_cap = {
   "appium:deviceName": "R3CM906NL0V",
@@ -17,9 +18,9 @@ driver.implicitly_wait(30)
 driver.find_element(By.ID, 'com.sand.airmirror:id/tvLogin').click()
 
 search_element_id = driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout/android.widget.AutoCompleteTextView')
-search_element_id.set_text('ID')
+search_element_id.set_value(GOOGLE_ID)
 
 search_element_password = driver.find_element(By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.EditText')
-search_element_password.set_text('PASSWORD')
+search_element_password.set_value(GOOGLE_PASSWORD)
 
 driver.find_element(By.ID, 'com.sand.airmirror:id/btnLogin').click()
