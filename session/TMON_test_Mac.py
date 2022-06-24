67301cab68fe0cf2d36a7218ba8133093f08d5b1
yooinbeom <yib950@gmail.com>
2022-06-24 16:55:45 +0900

    add Mac and Window files for desired cap

--- session/TMON_test_Mac.py
+++ session/TMON_test_Mac.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 desired_cap = {
     "appium:deviceName": "Android Emulator",
     "platformName": "Android",
-    "appium:app": "D:/util/ticketmonster_v5.7.5.apk",
+    "appium:app": "/Users/yooinbeom/Downloads/apk/ticketmonster_v5.7.5.apk",
     "appPackage": "com.tmon",
     "appWaitActivity": "com.tmon.main.MainActivity"
 }
@@ -21,14 +21,15 @@ driver.find_element(By.ID, "com.tmon:id/btn_confirm").click()
 driver.find_element(By.ID, "com.android.permissioncontroller:id/permission_deny_button").click()
 driver.find_element(By.ID, "com.tmon:id/btn_cancel").click()
 
-"""search"""
+"""Search"""
 driver.find_element(By.ID, "com.tmon:id/keyword").click()
 driver.find_element(By.XPATH, "//android.widget.EditText[@resource-id='search_input']").click()
 driver.find_element(By.XPATH, "//android.widget.EditText[@resource-id='search_input']").set_value("iphone")
 driver.find_element(By.XPATH, "//android.widget.Button[@text='검색']").click()
 
+"""Take Screenshot"""
 ts = time.strftime("%Y_%m_%d_%H%M%S")
 activityname = driver.current_activity
 filename = activityname+ts
 time.sleep(3)
-driver.save_screenshot("C:/Users/inbeom/PycharmProjects/AppiumSandbox/Screenshots/"+filename+".png")
+driver.save_screenshot("/Users/yooinbeom/PycharmProjects/AppiumSandbox/Screenshots/"+filename+".png")
---
+++ session/TMON_test_Window.py
@@ -0,0 +1,35 @@
+import time
+
+from appium import webdriver
+from selenium.webdriver.common.by import By
+
+desired_cap = {
+    "appium:deviceName": "Android Emulator",
+    "platformName": "Android",
+    "appium:app": "D:/util/ticketmonster_v5.7.5.apk",
+    "appPackage": "com.tmon",
+    "appWaitActivity": "com.tmon.main.MainActivity"
+}
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
+driver.implicitly_wait(30)
+
+"""Permission Deny"""
+driver.find_element(By.ID, "com.tmon:id/btn_do_not_receive").click()
+driver.find_element(By.ID, "com.tmon:id/slimNavibarClose").click()
+driver.find_element(By.ID, "com.tmon:id/btn_confirm").click()
+driver.find_element(By.ID, "com.android.permissioncontroller:id/permission_deny_button").click()
+driver.find_element(By.ID, "com.tmon:id/btn_cancel").click()
+
+"""Search"""
+driver.find_element(By.ID, "com.tmon:id/keyword").click()
+driver.find_element(By.XPATH, "//android.widget.EditText[@resource-id='search_input']").click()
+driver.find_element(By.XPATH, "//android.widget.EditText[@resource-id='search_input']").set_value("iphone")
+driver.find_element(By.XPATH, "//android.widget.Button[@text='검색']").click()
+
+"""Take Screenshot"""
+ts = time.strftime("%Y_%m_%d_%H%M%S")
+activityname = driver.current_activity
+filename = activityname+ts
+time.sleep(3)
+driver.save_screenshot("C:/Users/inbeom/PycharmProjects/AppiumSandbox/Screenshots/"+filename+".png")
